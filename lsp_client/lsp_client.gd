80a5c37f938b95d50f1f3ad6d8e2516a7e09157b
mkh-user <khalili1388mahan@gmail.com>
2025-12-18 09:05:15 +0330

    Improve client

--- lsp_client/lsp_client.gd
+++ lsp_client/lsp_client.gd
@@ -4,37 +4,51 @@ extends Node
 signal connection_failed(err: Error)
 signal connecting(host: String, port: int)
 signal connected
-signal response(response: String)
+signal response(resp: String)
+signal message_sent(msg: String)
 
 var connection := StreamPeerTCP.new()
 var message_id := 1
 
 func _process(_delta):
+	if connection.get_status() in [StreamPeerTCP.STATUS_ERROR, StreamPeerTCP.STATUS_NONE]:
+		return
 	connection.poll()
 	if connection.get_available_bytes() > 0:
 		var resp = connection.get_utf8_string(connection.get_available_bytes())
-		response.emit(resp)
+		print("[LSPClient] Response passed to client")
+		var resps := resp.split("Content-Length:", false)
+		for r in resps:
+			response.emit("Content-Length:" + r)
 
 
 func connect_to_server(host: String, port: int) -> void:
+	print("[LSPClient] Connection requested: " + host + ":" + str(port))
 	var err = connection.connect_to_host(host, port)
 	if err:
 		connection_failed.emit(err)
+		print("[LSPClient] Connection failed: " + error_string(err))
 	else:
 		connecting.emit(host, port)
+		var timeout := get_tree().create_timer(10)
 		while connection.get_status() == StreamPeerTCP.STATUS_CONNECTING:
 			await get_tree().create_timer(0.1).timeout
+			if timeout.time_left == 0.0:
+				connection_failed.emit(ERR_TIMEOUT)
+				print("[LSPClient] Connection timeout")
+				return
 		connected.emit()
+		print("[LSPClient] Connected to host")
 
 
-func send_initialize() -> void:
+func send_initialize(root_uri: String) -> void:
 	var msg := {
 		"jsonrpc": "2.0",
 		"id": message_id,
 		"method": "initialize",
 		"params": {
 			"processId": null,
-			"rootUri": "file:///" + OS.get_cache_dir().uri_encode(),
+			"rootUri": "file:///" + root_uri,
 			"capabilities": {}
 		}
 	}
@@ -61,6 +75,8 @@ func send_did_open(uri: String, language_id: String, text: String) -> void:
 func send_message(msg: Dictionary) -> void:
 	var json := JSON.stringify(msg)
 	var header := "Content-Length: %d\r\n\r\n" % json.to_utf8_buffer().size()
+	print("[LSPClient] Send message to server: " + msg["method"])
 	#print("Send message to LSP: " + header + json)
 	var ful_msg := header.to_ascii_buffer() + json.to_utf8_buffer()
 	connection.put_data(ful_msg)
+	message_sent.emit(header + json)
--- main.gd
+++ main.gd
@@ -1,9 +1,23 @@
 extends Control
 
+@export var client_masseges: RichTextLabel
+@export var server_messages: RichTextLabel
+
 func _ready() -> void:
 	var client = LSPClient.new()
 	add_child(client)
-	client.connecting.connect(print)
-	client.connected.connect(client.send_initialize)
-	client.response.connect(print)
-	client.connect_to_server("127.0.0.1", 6005)
+	client.connected.connect(_append_cm.bind("Connected"))
+	client.connecting.connect(_append_cm.bind("Connecting...").unbind(2))
+	client.connection_failed.connect(_append_cm.bind("Connection failed!").unbind(1))
+	client.message_sent.connect(_append_cm)
+	client.response.connect(_append_sm)
+	client.connected.connect(client.send_initialize.bind(ProjectSettings.globalize_path("res://")))
+	client.connect_to_server("127.0.0.1", 2087)
+
+
+func _append_cm(text: String) -> void:
+	client_masseges.text += "Client: \n" + text
+
+
+func _append_sm(text: String) -> void:
+	server_messages.text += "Server: \n" + text
